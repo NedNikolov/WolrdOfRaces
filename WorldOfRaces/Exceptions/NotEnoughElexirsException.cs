9cdca1304affcd61a6dd50ceb2bdd5a7dd8a6f84
Ralitsa Voronevska <ralicavoronevska@gmail.com>
2015-12-14 20:16:46 +0200

    exceptions initializing

---
+++ WorldOfRaces/Exceptions/NotEnoughElexirsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldOfRaces.Exceptions
+{
+    public class NotEnoughElexirsException : Exception
+    {
+        public NotEnoughElexirsException(string msg)
+            : base(msg)
+        {
+        }
+    }
+}
---
+++ WorldOfRaces/Exceptions/ObjectOutsideOfMapException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldOfRaces.Exceptions
+{
+    public class ObjectOutsideOfMapException : Exception
+    {
+        public ObjectOutsideOfMapException(string message)
+            : base(message)
+        {
+        }
+    }
+}
