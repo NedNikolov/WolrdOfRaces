a1088ef11f90b5789fe3f647c099d23421233dbb
gergana6750 <gergana.bor@feg.bg>
2015-12-14 23:36:09 +0200

    Added enum HeroRace

---
+++ WorldOfRaces/Characters/HeroRace.cs
@@ -0,0 +1,12 @@
+﻿namespace WorldOfRaces.Characters
+{
+    public enum HeroRace
+    {
+        Archer = 1,
+        CrossBowMan = 2,
+        Warrior = 3,
+        Tank = 4,
+        HeavyTank = 5,
+        Knight = 6,
+    }
+}
