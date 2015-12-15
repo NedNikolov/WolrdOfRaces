1ecd93a285d3b8930485d532b79806eb8799a6a1
eakshark <aleksandar.raichev@gmail.com>
2015-12-15 15:40:02 +0200

    elf

---
+++ WorldOfRaces/Characters/NightElf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldOfRaces;
+using WorldOfRaces.Attributes;
+using WorldOfRaces.Characters;
+
+[Enemy]
+    public class NightElf : Character
+    {
+        private const int NightElfDamage = 90;
+        private const int NightElfHealth = 110;
+        private const char NightElffSymbol = 'N';
+
+        public NightElf(Position position, string name)
+            : base(position, NightElffSymbol, name, NightElfDamage, NightElfHealth)
+        {
+        }          
+    }
+}
