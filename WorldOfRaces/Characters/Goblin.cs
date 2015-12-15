23ae9752d6436d68e94a69cf618e168572471d46
eakshark <aleksandar.raichev@gmail.com>
2015-12-15 15:30:04 +0200

    goblin

---
+++ WorldOfRaces/Characters/Goblin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldOfRaces;
+using WorldOfRaces.Attributes;
+using WorldOfRaces.Characters;
+
+ [Enemy]
+    public class Goblin : Character
+    {
+        private const int GoblinDamage = 100;
+        private const int GoblinHealth = 250;
+        private const char GoblinSymbol = 'G';
+
+        public Goblin(Position position, string name)
+            : base(position, GoblinSymbol, name, GoblinDamage, GoblinHealth)
+        {
+        }
+    }
+}
