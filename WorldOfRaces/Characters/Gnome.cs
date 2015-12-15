073425727f7ed96b59c0a801d8edbb174041e141
eakshark <aleksandar.raichev@gmail.com>
2015-12-15 15:25:28 +0200

    new

---
+++ WorldOfRaces/Characters/Gnome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldOfRaces;
+using WorldOfRaces.Attributes;
+using WorldOfRaces.Characters;
+
+[Enemy]
+    public class Gnome : Character
+    {
+        private const int GnomeDamage = 120;
+        private const int GnomeHealth = 260;
+        private const char GnomefSymbol = 'M';
+
+        public Gnome(Position position, string name)
+            : base(position, GnomefSymbol, name, GnomeDamage, GnomeHealth)
+        {
+        }
+    }
+}
