cc72c6d544a780a34a9763ad3dab74efabebbe30
eakshark <aleksandar.raichev@gmail.com>
2015-12-15 15:22:31 +0200

    NewDwarf

---
+++ WorldOfRaces/Characters/Dwarf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldOfRaces;
+using WorldOfRaces.Attributes;
+using WorldOfRaces.Characters;
+
+    [Enemy]
+    public class Dwarf : Character
+    {
+        private const int DwarfDamage = 200;
+        private const int DwarfHealth = 250;
+        private const char DwarfSymbol = 'D';
+
+        public Dwarf(Position position, string name)
+            : base(position, DwarfSymbol, name, DwarfDamage, DwarfHealth)
+        {
+        }
+    }
+}
