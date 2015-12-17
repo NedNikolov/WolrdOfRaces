5dc29119a32af98c1cd7287707b36f041023f954
galin-s <galinstef@outlook.com>
2015-12-17 13:18:14 +0200

    update

---
+++ WorldOfRaces/Characters/Orc.cs
@@ -0,0 +1,17 @@
+﻿namespace WorldOfRaces.Characters
+{
+    using Attributes;
+
+    [Enemy]
+    public class Orc : Character
+    {
+        private const int OrcDamage = 300;
+        private const int OrcHealth = 800;
+        private const char OrcfSymbol = 'O';
+
+        public Orc(Position position, string name)
+            : base(position, OrcfSymbol, name, OrcDamage, OrcHealth)
+        {
+        }
+    }
+}
---
+++ WorldOfRaces/Characters/Tauren.cs
@@ -0,0 +1,17 @@
+﻿namespace WorldOfRaces.Characters
+{
+    using Attributes;
+
+    [Enemy]
+    public class Tauren : Character
+    {
+        private const int TaurenDamage = 250;
+        private const int TaurenHealth = 900;
+        private const char TaurenfSymbol = 'T';
+
+        public Tauren(Position position, string name)
+            : base(position, TaurenfSymbol, name, TaurenDamage, TaurenHealth)
+        {
+        }
+    }
+}
